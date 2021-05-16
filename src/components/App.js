13a8aa24db4f94b9de7057535f3ccc0786858aa6
sehdev kumar <55868273+sehdevkumar@users.noreply.github.com>
2021-05-16 18:57:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,56 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+   
+    constructor(props){
+      super(props)
+      this.date = new Date();
+      this.hours = this.date.getHours();
+      this.minutes = this.date.getMinutes();
+      this.seconds = this.date.getSeconds();
+      this.ampm = this.hours >= 12 ? 'PM' : 'AM';
+      this.hours = this.hours % 12;
+      this.hours = this.hours ? this.hours : 12;
+     
+      this.state={
+        hour:this.hours,
+        minute:this.minutes,
+        second:this.seconds,
+        amORpm:this.ampm
+
+      }
+    }
     render() {
 
         return(
             <>
-               
+               <div id="time">{this.state.hour+":"+this.state.minute+":"+this.state.second+" "+this.state.amORpm}</div>
             </>
         )
     }
+  
+    componentDidMount(){
+       this.myinterVal = setInterval(()=>{
+          this.date = new Date();
+          this.hours = this.date.getHours();
+          this.minutes = this.date.getMinutes();
+          this.seconds = this.date.getSeconds();
+          this.ampm = this.hours >= 12 ? 'PM' : 'AM';
+          this.hours = this.hours % 12;
+          this.hours = this.hours ? this.hours : 12;
+         this.setState(prev=>({
+            hour:this.hours,
+            minute:this.minutes,
+            second:this.seconds,
+            amORpm:this.ampm
+         }))
+       },1000);
+    }
+
+    componentWillUnmount(){
+      clearInterval(this.myinterVal);
+    }
+
 }
 
 
